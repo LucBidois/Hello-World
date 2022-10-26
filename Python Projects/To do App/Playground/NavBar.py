7541192055ca2208f583324d45638291b5174bfd
Luc <15036309+LucBidois@users.noreply.github.com>
2022-10-26 17:21:57 +0100

    Add files via upload

---
+++ Python Projects/To do App/Playground/NavBar.py
@@ -0,0 +1,9 @@
+"""
+A function that when run, displays the navigation bar and connected buttons
+"""
+
+def 
+pygame.draw.rect(gameDisplay, (0, 0, 0), (200, 200, 100, 100))
+
+
+Pg_Ess.button("Sup me dudes", 100, 100, 100, 100, (100, 100, 100), (200, 200, 200))

c748f59cb2e77717371ca75c1010effec8acddf8
Luc <15036309+LucBidois@users.noreply.github.com>
2022-10-27 17:04:49 +0100

    Add files via upload

---
+++ Web Projects/Timezones/Timezones.js
@@ -0,0 +1,19 @@
+function timezone() {
+  var d = new Date();
+  var hour = date.gethours();
+  var min = date.getminutes();
+  var x = document.getElementsByClassName("UK");
+  var p = document.createElement("p"); // creating new paragraph
+  var node = document.createTextNode(d);
+  p.appendChild(node); // adding text to the paragraph
+  var div = document.getElementsByClassName("UK");
+  alert(div);
+  div.appendChild(p); //adding paragraph to desired element
+}
+var q = new Date();
+document.write(q);
+
+
+date.gethours();
+date.getminutes();
+date.getseconds();
